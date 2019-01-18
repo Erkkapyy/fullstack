e29284bf1d1180e75a2e42ff6526819c9b9137f5
Erkkapyy <erkka.pyykonen@gmail.com>
2019-01-18 15:48:12 +0200

    Toisen viikon aloittelua

---
+++ Week 2/tehtava2/src/index.js
@@ -0,0 +1,73 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Header = props => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+const Content = props => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = props => {
+  return (
+    <div>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Total = props => {
+  return (
+    <div>
+      <p>
+        Yhteensä{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}{" "}
+        tehtävää
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: "Half Stack - sovelluskehitys",
+    parts: [
+      {
+        name: "Reactin perusteet",
+        exercises: 10
+      },
+      {
+        name: "Reactin perusteet",
+        exercises: 7
+      },
+      {
+        name: "Komponenttien tila",
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById("root"))
